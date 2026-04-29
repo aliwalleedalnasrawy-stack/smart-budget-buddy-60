6c4e418e2efa66af0cfe76bcae85150578d05569
aliwalleedalnasrawy-stack <aliwalleedalnasrawy@gmail.com>
2026-04-29 13:02:12 +0300

    Create client.tsx

---
+++ src/client.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+import "./styles.css";
+
+const router = createRouter({ routeTree });
+
+const rootElement = document.getElementById("root")!;
+createRoot(rootElement).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
