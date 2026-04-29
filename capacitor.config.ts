0ca6c2547c1a101ff4d06df47aa79052639fa0ea
aliwalleedalnasrawy-stack <aliwalleedalnasrawy@gmail.com>
2026-04-29 14:51:45 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,10 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.smartbudget.buddy',
+  appId: 'com.yourname.walletapp',
   appName: 'ميزانيتي الذكية',
-  webDir: 'dist/client',
+  webDir: 'dist/mobile',   // ← تغيير من dist/client إلى dist/mobile
+  bundledWebRuntime: false,
   android: {
     allowMixedContent: true,
   },
