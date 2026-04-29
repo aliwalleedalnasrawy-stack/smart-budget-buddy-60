2c88c4ddf9d3e4e24cbd40ccdf42600228da67c5
aliwalleedalnasrawy-stack <aliwalleedalnasrawy@gmail.com>
2026-04-29 15:16:23 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.yourname.walletapp',
   appName: 'ميزانيتي الذكية',
-  webDir: 'dist',
+  webDir: 'dist-mobile',
   bundledWebRuntime: false,
   android: {
     allowMixedContent: true,
